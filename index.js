de2d6dc436852b8a31d59405f93c7aa1650a94ef
m8dotpie <alentev.olm@ya.ru>
2021-03-11 19:39:41 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const bot = new Composer();
 
 const dbURL = process.env.DATABASE_URL;
 
+console.log(dbURL);
+console.log(process.env.PGHOST);
+
 bot.hears('test', (ctx) => {
     const pool = new Pool();
     pool.query('SELECT NOW()', (err, res) => {
         console.log(err, res);
         pool.end();
     });
-    console.log(dbURL);
-    console.log(process.env.PGHOST);
     ctx.telegram.sendMessage(ctx.chat.id, 'Alive and logged');
 });
 
