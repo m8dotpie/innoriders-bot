2c302a9a3096063ab4ade95212804051cc80d845
m8dotpie <alentev.olm@ya.ru>
2021-03-11 22:50:43 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -31,14 +31,17 @@ client.query(`CREATE TABLE IF NOT EXISTS ${curTable} (id integer, addingTraining
 });
 
 bot.start(async (ctx) => {
-    console.log((await client.query(`SELECT * FROM ${curTable} WHERE id=${ctx.from.id}`)).rows);
-    // client.query(`INSERT INTO ${curTable} (id, addingTraining, nextProof) VALUES (${ctx.from.id}, false, 0)`, (err, res) => {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log(res);
-    //     }
-    // });
+    if ((await client.query(`SELECT * FROM ${curTable} WHERE id=${ctx.from.id}`)).rows.length != 0) {
+        ctx.reply("You already in da club!");
+    } else {
+        client.query(`INSERT INTO ${curTable} (id, addingTraining, nextProof) VALUES (${ctx.from.id}, false, 0)`, (err, res) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(res);
+            }
+        }).then(() => ctx.reply("Welcome to the club, mate!"));
+    }
 });
 
 module.exports = bot;
