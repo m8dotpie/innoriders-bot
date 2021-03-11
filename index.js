5e9eaf6085c080561930456d5a4406a90e4702b5
m8dotpie <alentev.olm@ya.ru>
2021-03-11 20:18:13 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -12,19 +12,11 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT * FROM pg_catalog.pg_tables;', (err, res) => {
+client.query('CREATE TABLE IF NOT EXISTS testData (addingTraining bool, proofsIDs integer[10], nextProof integer)', (err, res) => {
     if (err) {
-        console.log(err);
-    } else {
-        console.log(res);
+        throw err;
     }
+    console.log(res);
 });
-
-// client.query('CREATE TABLE IF NOT EXISTS proofData (addingTraining bool, proofsIDs integer[10], nextProof integer)', (err, res) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log(res);
-// });
 
 module.exports = bot;
