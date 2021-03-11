0b0793fa1d55373010167eba4936b42ac8d31733
m8dotpie <alentev.olm@ya.ru>
2021-03-11 19:55:06 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ const client = new Client({
 
 client.connect();
 
-client.query('CREATE TABLE IF NOT EXISTS proofData (addingTraining bool, proofsIDs integer[10])');
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
+client.query('CREATE TABLE IF NOT EXISTS proofData (addingTraining bool, proofsIDs integer[10])', (err, res) => {
+    if (err) {
+        throw err;
     }
-    client.end();
+    console.log(res);
 });
 
 module.exports = bot;
