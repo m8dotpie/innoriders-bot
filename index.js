4f9cf64b030955c936c27847820c589ccba92705
m8dotpie <alentev.olm@ya.ru>
2021-03-11 19:44:02 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const { Client } = require('pg');
 
 const bot = new Composer();
 
-const { Client } = require('pg');
-
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
