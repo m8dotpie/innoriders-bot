de0b0e4e01b4da6ac9f8a33b9a45952309a6f67c
m8dotpie <alentev.olm@ya.ru>
2021-03-11 17:50:58 +0300

    Testing env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Composer } = require('micro-bot');
 
 const bot = new Composer();
 
-bot.hears('test', ({reply}) {
+bot.hears('test', ({reply}) => {
     console.log(process.env.DATABASE_URL);
     reply('Logged');
 });
