864128c16a0d727081f0e9aa69b40a6b6a1d2262
m8dotpie <alentev.olm@ya.ru>
2021-03-11 17:58:23 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const { Composer } = require('micro-bot');
 
 const bot = new Composer();
 
-bot.hears('test', () => console.log(process.env.DATABASE_URL));
+bot.hears('test', (ctx) => {
+    console.log(process.env.DATABASE_URL);
+    ctx.telegram.sendMessage(ctx.chat.id, 'Alive and logged');
+});
