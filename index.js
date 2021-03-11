1d51aea4c5fcc5d6387fdb1ba7c0f5a047d3cc43
m8dotpie <alentev.olm@ya.ru>
2021-03-11 23:57:45 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ async function startTraining(ctx) {
     const trainingMenu = Telegraf.Extra
           .markdown()
           .markup((m) => m.keyboard([
-              [m.keyboardButton('Finished with proofs', 'sendTraining')],
-              [m.keyboardButton('Forget about this training', 'removeTraining')]
+              [m.button('Finished with proofs', 'sendTraining')],
+              [m.button('Forget about this training', 'removeTraining')]
           ]));
     await client.query(`UPDATE $curTable (addingTraining) VALUES (true) WHERE id=${ctx.from.id}`);
     ctx.reply('Waiting for proofs, bro!');
@@ -57,7 +57,7 @@ bot.start(async (ctx) => {
     const trainingMenu = Telegraf.Extra
           .markdown()
           .markup((m) => m.keyboard([
-              [m.keyboardButton('Add training', 'addTraining')]
+              [m.button('Add training', 'addTraining')]
           ]));
     if ((await client.query(`SELECT * FROM ${curTable} WHERE id=${ctx.from.id}`)).rows.length != 0) {
         ctx.reply('You already in da club!');
