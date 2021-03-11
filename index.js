cf68237d898540774d0a234cc101e3884c455b3e
m8dotpie <alentev.olm@ya.ru>
2021-03-11 17:55:16 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { Composer } = require('micro-bot');
 
 const bot = new Composer();
 
-bot.hears('test', ({reply}) => console.log(process.env.DATABASE_URL));
+bot.hears('test', () => console.log(process.env.DATABASE_URL));
