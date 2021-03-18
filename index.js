ae3fefdc9d276e824883cf93e22e43a06af2c444
m8dotpie <alentev.olm@ya.ru>
2021-03-18 17:11:47 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -93,8 +93,7 @@ bot.hears('Finished with proofs', async (ctx) => {
 bot.action('approved', (ctx) => ctx.deleteMessage());
 
 bot.action(RegExp('^(approve)\/*'), async (ctx) => {
-    console.log(ctx);
-    ctx.sendMessage(process.env.APPROVE_CHAT, ctx.message.text,
+    ctx.sendMessage(process.env.APPROVE_CHAT, ctx.update.message.text,
                     {
                         reply_markup: {
                             inline_keyboard: [
