49f4c6b225bd9209541aba871eec72a58f018a40
m8dotpie <alentev.olm@ya.ru>
2021-03-18 15:58:06 +0300

    Auto deploy

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ bot.command('/notify', async (ctx) => {
 
 bot.on(['photo', 'video', 'document'], async (ctx) => {
     let proofs = (await client.query(`SELECT proofs FROM ${curTable} WHERE id=${ctx.from.id}`)).rows;
+    console.log(await client.query(`SELECT proofs FROM ${curTable} WHERE id=${ctx.from.id}`));
     if (proofs.length == 0) {
         proofs = [ctx.message.message_id];
     } else {
